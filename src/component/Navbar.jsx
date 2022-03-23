abf6b2881200dacbed10862f5e2d5565fd74b093
riveenpesara <liyanaget333@gmail.com>
2022-03-24 04:20:49 +0530

    Newly pushed code

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+
+    const state = useSelector((state)=> state.handleCart)
+
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+                <div class="container">
+                    <a class="navbar-brand fw-bold fs-4" href="/">NIRVANA CLOTHING</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/products">Products</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="/about">About</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="/contact">Contact</a>
+                            </li>
+                        </ul>
+                        <div className="buttons">
+                            <a href="/login" className="btn btn-outline-dark">
+                                <i className="fa fa-sign-in me-1"></i> Login
+                            </a>
+                            <a href="/register" className="btn btn-outline-dark ms-2">
+                                <i className="fa fa-user-plus me-1"></i> Register
+                            </a>
+                            <a href="/cart" className="btn btn-outline-dark ms-2">
+                                <i className="fa fa-shopping-cart me-1"></i> Cart ({state.length})
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
